6218393b7c1f7df3a2c7926586e7f4dba8e5328a
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-07-31 19:31:11 -0400

    Remove unnecessary files

--- dbo.UserAuth.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[userAuth]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [userID] INT NULL, 
-    [UserName] VARCHAR(50) NULL, 
-    [Email] VARCHAR(50) NULL, 
-    [Password] VARCHAR(50) NULL
-)
