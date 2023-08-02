0bb42f0faa9d2db9c82fa849f5fdaaacc1bf694f
Riccardo Reali <rreali@my.centennialcollege.ca>
2023-08-02 11:34:02 -0400

    Added an installer for distribution

--- WPFTutorial/obj/Debug/WPFTutorial_Content.g.i.cs
+++ WPFTutorial/obj/Debug/WPFTutorial_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("db%202.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("logodb.ico")]
 
 
